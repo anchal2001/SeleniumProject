4f95c48f9b241fd84e4d7612f8e7666c629d96e5
Anchal <anchalg250@gmail.com>
2025-01-03 19:41:14 +0530

    commit

--- src/main/java/org/example/ActionFiles/OrangeHRMEleActions.java
+++ src/main/java/org/example/ActionFiles/OrangeHRMEleActions.java
@@ -1,5 +1,54 @@
 package org.example.ActionFiles;
 
+import org.example.XpathLocator.OrangeHRMLocators;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class OrangeHRMEleActions {
-    
+
+    private WebDriver driver;
+
+    public OrangeHRMEleActions(WebDriver driver)
+    {
+        this.driver = driver;
+        
+    }
+
+    public void OpenApplication(String url)
+    {
+        driver.get(url);
+        driver.manage().window().maximize();
+        System.out.println("Browser title is: " + driver.getTitle());
+    }
+
+    public void enterUsername(String username)
+    {
+        WebElement usernameField = driver.findElement(By.xpath(OrangeHRMLocators.USERNAME_FIELD));
+        usernameField.click();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password)
+    {
+        WebElement passwordField = driver.findElement(By.xpath(OrangeHRMLocators.PASSWORD_FIELD));
+        passwordField.click();
+        passwordField.sendKeys(password);
+    }
+    public void clickLoginBtn()
+    {
+        WebElement loginbtn = driver.findElement(By.xpath(OrangeHRMLocators.LOGIN_BUTTON));
+        loginbtn.click();
+    }
+    public String getUsernameFieldValue() {
+        WebElement usernameField = driver.findElement(By.xpath(OrangeHRMLocators.USERNAME_FIELD));
+        return usernameField.getAttribute("value");
+    }
+
+    public String getPasswordFieldValue() {
+        WebElement passwordField = driver.findElement(By.xpath(OrangeHRMLocators.PASSWORD_FIELD));
+        return passwordField.getAttribute("value");
+    }
+
 }
+
--- src/main/java/org/example/MainMethod/OrangeHRMMain.java
+++ src/main/java/org/example/MainMethod/OrangeHRMMain.java
@@ -1,5 +1,6 @@
 package org.example.MainMethod;
 
+import org.example.ActionFiles.OrangeHRMEleActions;
 import org.example.XpathLocator.OrangeHRMLocators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,32 +11,32 @@ import java.util.concurrent.TimeUnit;
 
 public class OrangeHRMMain {
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver","driver//chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        driver.manage().window().maximize();
+
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        OrangeHRMEleActions actions = new OrangeHRMEleActions(driver);
+        actions.OpenApplication("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.manage().window().maximize();
+
         System.out.println("Browser title is : " + driver.getTitle());
 
-        WebElement usernameField = driver.findElement(By.xpath(OrangeHRMLocators.USERNAME_FIELD));
-        usernameField.click();
-        usernameField.sendKeys("Admin");
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-        WebElement passwordField = driver.findElement(By.xpath(OrangeHRMLocators.PASSWORD_FIELD));
-        passwordField.click();
-        passwordField.sendKeys("admin123");
-
-        String username = usernameField.getAttribute("value");
-        String password = passwordField.getAttribute("value");
+        actions.enterUsername("Admin");
+        actions.enterPassword("admin123");
+        String username = actions.getUsernameFieldValue();
+        String password = actions.getPasswordFieldValue();
 
         System.out.println("Username and Password for the login are : " + username + " " + password);
 
-        WebElement loginbtn = driver.findElement(By.xpath(OrangeHRMLocators.LOGIN_BUTTON));
-        loginbtn.click();
+        actions.clickLoginBtn();
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+
+        driver.quit();
 
     }
 }
