fb6c4daa965bb9d1f2c6c465b207d3f13f41209a
Anchal Gupta <90039206+anchal2001@users.noreply.github.com>
2025-01-12 17:05:46 +0530

    Merge pull request #1
    * new changes_1
    
    * new changes_1
    
    * new changes_1
    
    * new changes_1
    
    * new changes_1
    
    * new changes_1

--- src/main/java/org/example/XpathLocator/OrangeHRMLocators.java
+++ src/main/java/org/example/XpathLocator/OrangeHRMLocators.java
@@ -6,5 +6,8 @@ public class OrangeHRMLocators {
     public static final String PASSWORD_FIELD = "//input[@placeholder=\"Password\"]";
     public static final String LOGIN_BUTTON = "//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button";
     public static final String ADMIN_TAB = "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span";
+    public static final String USERNAME_ADMIN_FIELD = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input";
+    public static final String USER_ROLE_DROPDOWN ="//div[@class='oxd-select-text oxd-select-text--active']//div[2]//i";
+    public static final String ROLE_DROPDOWN = "//div[@class='oxd-select-dropdown oxd-select-dropdown--visible']//span[text()='Admin']";
 
 }
